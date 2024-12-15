3aadcc39ee12e31d0793e22758e8345f0100cf70
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2024-12-16 00:16:32 +0300

    update

--- pages/popup/src/helpers/getCurrentTab.ts
+++ pages/popup/src/helpers/getCurrentTab.ts
@@ -1,9 +1,9 @@
 export default function getCurrentTab(): Promise<chrome.tabs.Tab> {
-  if (window.$tab) {
-    return Promise.resolve(window.$tab);
-  }
-
   return new Promise((resolve, reject) => {
+    if (window.$tab) {
+      return resolve(window.$tab);
+    }
+
     let retry = 0;
     const interval = setInterval(async () => {
       const [tab] = await chrome.tabs.query({
@@ -14,11 +14,11 @@ export default function getCurrentTab(): Promise<chrome.tabs.Tab> {
       if (tab) {
         clearInterval(interval);
         window.$tab = tab;
-        resolve(tab);
+        return resolve(tab);
       }
       if (retry >= 10) {
         clearInterval(interval);
-        reject('Tab not found');
+        return reject('Tab not found');
       }
       retry++;
     }, 100);
--- pages/popup/src/index.tsx
+++ pages/popup/src/index.tsx
@@ -3,6 +3,8 @@ import '@extension/ui/dist/global.css';
 import '@src/index.css';
 import Popup from '@src/Popup';
 import initTabSetup from '@src/helpers/initTabSetup';
+import * as Sentry from '@sentry/react';
+
 import './i18n';
 
 async function init() {
@@ -10,6 +12,13 @@ async function init() {
   if (!appContainer) {
     throw new Error('Can not find #app-container');
   }
+  Sentry.init({
+    dsn: 'https://d9c194b14eed5aa9c8278dd7316cb969@o4508474214514689.ingest.de.sentry.io/4508474223427664',
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
   const root = createRoot(appContainer);
   await initTabSetup();
   root.render(<Popup />);
